78738a4944549d5cc6f980510aedf5ef64291116
akugone <dev@akumaw.com>
2022-10-28 23:42:36 +0100

    Add ERC20 deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,6 +5,11 @@ async function main() {
   const CapazEscrowFactory = await ethers.getContractFactory('CapazEscrowFactory')
   const capazEscrowFactory = await CapazEscrowFactory.deploy()
   console.log('CapazEscrowFactory deployed to:', capazEscrowFactory.address)
+
+  // Deploy CapazEscrowFactory Contract
+  const SimpleERC20 = await ethers.getContractFactory('SimpleERC20')
+  const simpleERC20 = await SimpleERC20.deploy()
+  console.log('SimpleERC20 deployed to:', simpleERC20.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere

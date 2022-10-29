44cde9acab7110287295452005f3a2806fc3f446
akugone <dev@akumaw.com>
2022-10-29 10:13:30 +0100

    add CPZ token

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,10 +6,10 @@ async function main() {
   const capazEscrowFactory = await CapazEscrowFactory.deploy()
   console.log('CapazEscrowFactory deployed to:', capazEscrowFactory.address)
 
-  // Deploy CapazEscrowFactory Contract
-  const SimpleERC20 = await ethers.getContractFactory('SimpleERC20')
-  const simpleERC20 = await SimpleERC20.deploy()
-  console.log('SimpleERC20 deployed to:', simpleERC20.address)
+  // Deploy SimpleERC20 Contract
+  const CapazERC20 = await ethers.getContractFactory('CapazERC20')
+  const capazERC20 = await CapazERC20.deploy()
+  console.log('capazERC20 deployed to:', capazERC20.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere

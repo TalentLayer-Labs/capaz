9a9a22a32a5295d1c28a2e121aebbe0abf5afd27
akugone <dev@akumaw.com>
2022-10-29 11:03:45 +0100

    Change decimal CPZ creation

--- contracts/CapazERC20.sol
+++ contracts/CapazERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -7,11 +7,11 @@ contract CapazERC20 is ERC20 {
     address public owner;
 
     constructor() ERC20("Capaz", "CPZ") {
-        _mint(msg.sender, 100000);
+        _mint(msg.sender, 10000 * 10**18);
         owner = msg.sender;
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return 2;
+        return 18;
     }
 }

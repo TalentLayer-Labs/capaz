fb685d439d32ddaf6ace76341ad0f1eb78b38df1
mattiapomelli <mattiapomelli@gmail.com>
2022-10-29 16:18:23 +0100

    Remove comments from aave strategy

--- contracts/AaveStrategy.sol
+++ contracts/AaveStrategy.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IPool} from "@aave/core-v3/contracts/interfaces/IPool.sol";
@@ -23,29 +21,17 @@ contract AaveStrategy is IStrategy, Ownable {
             IERC20(token).approve(pool, type(uint256).max);
         }
 
-        console.log("Depositing to pool: ", pool);
-        console.log("Token: ", token);
-        console.log("Amount: ", amount);
         // Supply to AAVE
-        // IPool(pool).supply(token, amount, address(this), 0);
+        IPool(pool).supply(token, amount, address(this), 0);
     }
 
     function claim(address pool, address token, uint256 amount, address user) external onlyOwner {
-        console.log("Claiming from pool: ", pool);
-        console.log("Token: ", token);
-        console.log("Amount: ", amount);
-        console.log("User: ", user);
-
         // Withdraw from AAVE to the user
-        // IPool(pool).withdraw(token, amount, user);
+        IPool(pool).withdraw(token, amount, user);
     }
 
     function claimAll(address pool, address token, address user) external onlyOwner {
-        console.log("Claiming all from pool: ", pool);
-        console.log("Token: ", token);
-        console.log("User: ", user);
-
         // Withdraw from AAVE to the user
-        // IPool(pool).withdraw(token, type(uint256).max, user);
+        IPool(pool).withdraw(token, type(uint256).max, user);
     }
 }

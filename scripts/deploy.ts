3cee5a6f7f1e353ed61f84f24208401a6809436f
mattiapomelli <mattiapomelli@gmail.com>
2022-10-29 15:13:12 +0100

    Set strategy pools in deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,14 @@
 import { ethers } from 'hardhat'
+import { AAVE_POOL_ADDRESS } from '../constants/addresses'
 
 async function main() {
   // Deploy CapazEscrowFactory Contract
   const CapazEscrowFactory = await ethers.getContractFactory('CapazEscrowFactory')
   const capazEscrowFactory = await CapazEscrowFactory.deploy()
   console.log('CapazEscrowFactory deployed to:', capazEscrowFactory.address)
+
+  // Setup strategy pools
+  capazEscrowFactory.setStrategyPool(1, AAVE_POOL_ADDRESS)
 
   // Deploy CapazEscrowFactory Contract
   const SimpleERC20 = await ethers.getContractFactory('SimpleERC20')

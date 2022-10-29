8c20071c945bc875dd24ce77feacbb4f511da291
akugone <dev@akumaw.com>
2022-10-29 19:25:02 +0100

    Script 0 working

--- scripts/playground/0-createEscrowContract.ts
+++ scripts/playground/0-createEscrowContract.ts
@@ -19,41 +19,76 @@ async function main() {
   let aliceBalance = await CapazERC20.balanceOf(alice.address)
   console.log('Alice balance: ', aliceBalance)
 
-  //Alice approves CapazEscrowFactory to spend 100 tokens
+  // alice send token to Bob
+  const aliceTransfer = await CapazERC20.connect(alice).transfer(bob.address, ethers.utils.parseEther('10'))
+  aliceTransfer.wait()
+  // get the wallet bob balance
+  let bobBalance = await CapazERC20.balanceOf(bob.address)
+  console.log('Bob balance: ', bobBalance)
+
+  //Alice and approves CapazEscrowFactory to spend 100 tokens
   const aliceApprove = await CapazERC20.connect(alice).approve(
     capazEscrowFactoryLocalAddress,
-    ethers.utils.parseEther('10'),
+    ethers.utils.parseEther('1'),
   )
   await aliceApprove.wait()
+
+  const bobApprove = await CapazERC20.connect(bob).approve(capazEscrowFactoryLocalAddress, ethers.utils.parseEther('1'))
+  await bobApprove.wait()
 
   //Escrow will check how many token Alice and Bob gave him permission to spend
   const aliceAllowance = await CapazERC20.allowance(alice.address, capazEscrowFactoryLocalAddress)
   console.log('Alice allowance: ', aliceAllowance.toString())
 
-  // get balance from Alice wallet
+  const bobAllowance = await CapazERC20.allowance(bob.address, capazEscrowFactoryLocalAddress)
+  console.log('Bob allowance: ', bobAllowance.toString())
+
+  // get balance from Alice and Bob Wallet
   aliceBalance = await CapazERC20.balanceOf(alice.address)
   console.log('Alice Balance: ', aliceBalance.toString())
 
-  const startTime = Math.floor(new Date().getTime() / 1000 + 20) // now + 20 seconds
-  const amount = ethers.utils.parseEther('0.1')
+  bobBalance = await CapazERC20.balanceOf(bob.address)
+  console.log('Bob Balance: ', bobBalance.toString())
+
+  const startTime = Math.floor(new Date().getTime() / 1000 + 2000) // now + 20 seconds
+
+  const amount = ethers.utils.parseEther('1')
+  console.log('Amount: ', amount.toString())
 
   // We mint a new escrow contract from CapazEscrowFactory
   const aliceMintData = await CapazEscrowFactoryContract.mint({
     sender: alice.address,
     receiver: carol.address,
-    tokenAddress: '0x48C45A025D154b40AffB41bc3bDEecb689edE7E6',
+    tokenAddress: '0x1eC0abD9539638FDb05EeD904Ca6F617BfBD6DCC',
     totalAmount: amount,
     startTime,
     periodDuration: 5,
-    periods: 2,
+    periods: 100,
     yieldStrategyId: 2,
     escrowAddress: '0x0000000000000000000000000000000000000000',
   })
   aliceMintData.wait()
 
+  // We mint a new escrow contract from CapazEscrowFactory
+  const bobMintData = await CapazEscrowFactoryContract.mint({
+    sender: bob.address,
+    receiver: dave.address,
+    tokenAddress: '0x1eC0abD9539638FDb05EeD904Ca6F617BfBD6DCC',
+    totalAmount: amount,
+    startTime,
+    periodDuration: 5,
+    periods: 2,
+    yieldStrategyId: 3,
+    escrowAddress: '0x0000000000000000000000000000000000000000',
+  })
+  bobMintData.wait()
+
   // We get the escrow data from the id to check if we get the right data
   const getCarolMintedNumber = await CapazEscrowFactoryContract.balanceOf(carol.address)
   console.log('Carol NFT number', getCarolMintedNumber)
+
+  const getDaveEscrowData = await CapazEscrowFactoryContract.balanceOf(dave.address)
+  console.log('Dave NFT number', getDaveEscrowData)
 }
 
 // We recommend this pattern to be able to use async/await everywhere

fc5c9ecc02c9b2a670dcafadb0e84e2127502de7
Romain <rm@adversport.com>
2022-10-30 02:30:42 +0100

    Integrate actions button into PayementRow

--- src/components/HomePayments.tsx
+++ src/components/HomePayments.tsx
@@ -26,6 +26,8 @@ export default function HomePayments() {
 
       const totalSupply: BigNumber = await contract.totalSupply();
 
+      if (totalSupply.toNumber() === 0) return;
+
       const userPayments: Payment[] = [];
 
       for (let tokenId = 0; tokenId < totalSupply.toNumber(); tokenId++) {
@@ -45,19 +47,19 @@ export default function HomePayments() {
     };
 
     getUserPayments();
-  }, [isAccountReady, config, provider, account]);
+  }, [isAccountReady, config, provider, account.address]);
 
   return (
     <main>
       {/* Main dashboard's table */}
       <div className='py-6 w-full'>
-        <div className='mx-auto xl:w-8/12 px-4 sm:px-6 md:px-8'>
+        <div className='mx-auto xl:w-10/12 px-4 sm:px-6 md:px-8'>
           <h1 className='text-2xl font-semibold text-gray-900'>Your payments</h1>
         </div>
 
         <div className='mx-auto px-4 sm:px-6 md:px-8'>
           <div className='py-4'>
-            <div className='w-full xl:w-8/12 mb-12 xl:mb-0 mx-auto mt-6'>
+            <div className='w-full xl:w-10/12 mb-12 xl:mb-0 mx-auto mt-6'>
               <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded '>
                 <div className='block w-full overflow-x-auto'>
                   <table className='items-center bg-transparent w-full border-collapse '>
@@ -82,7 +84,10 @@ export default function HomePayments() {
                           Yield
                         </th>
                         <th className='px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-50 py-5 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left'>
-                          More
+                          Releasable
+                        </th>
+                        <th className='px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-50 py-5 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left'>
+                          Action
                         </th>
                       </tr>
                     </thead>

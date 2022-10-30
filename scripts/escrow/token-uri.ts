a7d30d6dff9ed40d809361b590c905ded026db04
mattiapomelli <mattiapomelli@gmail.com>
2022-10-30 08:23:45 +0000

    Add token uri script

---
+++ scripts/escrow/token-uri.ts
@@ -0,0 +1,25 @@
+import { ethers } from 'hardhat'
+import { CAPAZ_ESCROW_FACTORY_ADDRESS } from '../../constants/addresses'
+
+async function main() {
+  // Deploy contract
+  const accounts = await ethers.getSigners()
+
+  console.log('Using address: ', accounts[0].address)
+
+  // Deploy escrow factory
+  const CapazEscrowFactory = await ethers.getContractFactory('CapazEscrowFactory')
+  const capazEscrowFactory = CapazEscrowFactory.attach(CAPAZ_ESCROW_FACTORY_ADDRESS)
+
+  const tokenId = 0
+  const tokenUri = await capazEscrowFactory.tokenURI(tokenId)
+
+  console.log('Token URI: ', tokenUri)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
